acf358464881aee3ce6486a9d114a3d8322547f4
Anonymous <alechelbling1@gmail.com>
2025-05-12 13:52:38 -0400

    Added nojekyll

--- diffusion-lab/svelte.config.js
+++ diffusion-lab/svelte.config.js
@@ -8,7 +8,7 @@ const config = {
     kit: {
         adapter: adapter(),
 		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/DiffusionLab' : '',
+            base: '/DiffusionLab'
         }
     }
 };
