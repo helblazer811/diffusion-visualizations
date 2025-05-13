b3306419f237d573ef35a1f8967dc5dd310d8753
Anonymous <alechelbling1@gmail.com>
2025-05-13 15:29:23 -0400

    Refactored title into its own component.

---
+++ diffusion-lab/src/lib/components/TitleBar.svelte
@@ -0,0 +1,95 @@
+
+<script>
+    import { base } from '$app/paths';
+</script>
+
+<style>
+    .title-container {
+        height: 70px;
+        width: 100%;
+        background-color: var(--dark-blue);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .title-inner-container {
+        height: 100%;
+        width: calc(var(--display-area-width) - 200px);
+        display: flex;
+        flex-direction: row;
+    }
+
+    .title {
+        height: 100%;
+        width: 50%;
+        margin: 0;
+        font-size: 2.8em;
+        color: white;
+        font-family: var(--font-family);
+        vertical-align: middle;
+        text-align: left;
+        display: flex;
+        align-items: center;
+        position: relative;
+    }
+
+    .title p {
+        font-weight: 200; /* Thin font */
+        margin: 0;
+    }
+
+    .sub-title {
+        /* width: var(--display-area-width); */
+        /* margin: 0 auto; */
+        margin: 0;
+        width: 50%;
+        font-size: 1.25em;
+        color: white;
+        font-family: var(--font-family);
+        font-weight: 200; /* Thin font */
+        /* Vertically align the text */
+        display: flex;
+        align-items: center;
+        text-align: right;
+    }
+
+    #github-logo {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-right: 10px;
+        position: absolute;
+        right: 0;
+        color: white;
+        font-size: 1.7em;
+        text-decoration: none;
+        font-family: var(--font-family);
+        font-weight: 200; /* Thin font */
+    }
+
+    #github-logo:hover {
+        text-decoration: underline;
+    }
+
+    #github-logo img {
+        width: 30px;
+        height: 30px;
+        margin-right: 10px;
+    }
+</style>
+
+<div class="title-container">
+    <div class="title-inner-container">
+        <div class="title">
+            <p><b>Diffusion</b>Lab</p>
+        </div>
+        <h1 class="sub-title"> 
+            Learn about diffusion and flow  models with interactive visualization. 
+        </h1>
+    </div>
+    <a href="https://github.com/helblazer811/DiffusionLab" id="github-logo" target="_blank">
+        <img src="{base}/github-logo.svg" alt="GitHub Logo"/>
+        Code
+    </a>
+</div>
--- diffusion-lab/src/routes/+page.svelte
+++ diffusion-lab/src/routes/+page.svelte
@@ -4,8 +4,6 @@
     // setWasmPaths('/tfjs-backend-wasm/');
     // import '@tensorflow/tfjs-backend-wasm'; // Import the WebGL backend for TensorFlow.js
 
-    import {base} from '$app/paths';
-
     import * as d3 from 'd3';
 
     import { onMount, onDestroy} from 'svelte';
@@ -28,6 +26,7 @@
     // Load up the application state
     import { UIState, model } from '$lib/state';
     // Load up the components
+    import TitleBar from '$lib/components/TitleBar.svelte';
     import TimeSlider from '$lib/components/time_slider/TimeSlider.svelte';
     import TrainingBar from '$lib/components/training_bar/TrainingBar.svelte';
     import DisplayArea from '$lib/components/display_area/DisplayArea.svelte';
@@ -255,80 +254,6 @@
         position: relative;
     }
 
-    .title-container {
-        height: 70px;
-        width: 100%;
-        background-color: var(--dark-blue);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .title-inner-container {
-        height: 100%;
-        width: calc(var(--display-area-width) - 200px);
-        display: flex;
-        flex-direction: row;
-    }
-
-    .title {
-        height: 100%;
-        width: 50%;
-        margin: 0;
-        font-size: 2.8em;
-        color: white;
-        font-family: var(--font-family);
-        vertical-align: middle;
-        text-align: left;
-        display: flex;
-        align-items: center;
-        position: relative;
-    }
-
-    .title p {
-        font-weight: 200; /* Thin font */
-        margin: 0;
-    }
-
-    .sub-title {
-        /* width: var(--display-area-width); */
-        /* margin: 0 auto; */
-        margin: 0;
-        width: 50%;
-        font-size: 1.25em;
-        color: white;
-        font-family: var(--font-family);
-        font-weight: 200; /* Thin font */
-        /* Vertically align the text */
-        display: flex;
-        align-items: center;
-        text-align: right;
-    }
-
-    #github-logo {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        margin-right: 10px;
-        position: absolute;
-        right: 0;
-        color: white;
-        font-size: 1.7em;
-        text-decoration: none;
-        font-family: var(--font-family);
-        font-weight: 200; /* Thin font */
-    }
-
-    #github-logo:hover {
-        text-decoration: underline;
-    }
-
-    #github-logo img {
-        width: 30px;
-        height: 30px;
-        margin-right: 10px;
-    }
-
     .main-area {
         /* width: 100%;
         background-color: white;
@@ -372,20 +297,7 @@
 </style>
 
 <div class="container">
-    <div class="title-container">
-        <div class="title-inner-container">
-            <div class="title">
-                <p><b>Diffusion</b>Lab</p>
-            </div>
-            <h1 class="sub-title"> 
-                Learn about diffusion and flow  models with interactive visualization. 
-            </h1>
-        </div>
-        <a href="https://github.com/helblazer811/DiffusionLab" id="github-logo" target="_blank">
-            <img src="{base}/github-logo.svg" alt="GitHub Logo"/>
-            Code
-        </a>
-    </div>
+    <TitleBar/>
     <TrainingBar/>
     <div class="main-area">
         <!-- <DatasetMenu datasetDict={datasetDict}/> -->
